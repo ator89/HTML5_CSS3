7549f628df5c99044cdea7d9980a55445dee2430
antox89 <ator89@outlook.com>
2019-04-09 21:22:46 -0600

    EventEmitter()

--- Node.js/EventEmitter.js
+++ Node.js/EventEmitter.js
@@ -1,5 +1,6 @@
 //Events
 var events = require('events');
+var util = require('util');
 
 //element.on('click',function function(){});
 
@@ -11,3 +12,25 @@ myEmitter.on('someEvent', function(mssg){
 });
 
 myEmitter.emit('someEvent', 'The event was emitted');
+
+//----------------------------
+
+var Person = function(name){
+  this.name = name;
+};
+
+util.inherits(Person, events.EventEmitter);
+
+var angel = new Person('james');
+var antonio = new Person('antonio');
+var torres = new Person('torres');
+var people = [angel,antonio,torres];
+
+people.forEach(function(person){
+  person.on('speak', function(mssg){
+    console.log(person.name + ' said: ' + mssg);
+  });
+});
+
+angel.emit('speak', 'hey');
+antonio.emit('speak', 'nooo');
