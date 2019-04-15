ae47ae085a9288b6325d2a8e15b28b7c2e76cbd0
antox89 <ator89@outlook.com>
2019-04-14 21:35:23 -0600

    NodeJS, Express and templates

--- NodeJS/App2/app.js
+++ NodeJS/App2/app.js
@@ -6,15 +6,30 @@ var express = require('express');
 //create object of express
 var app = express();
 
+
+app.set('view engine', 'ejs');
+
+
 app.get('/', function(req,res){
-  res.send('this is the homepage');
+  //Send a string
+  //res.send('this is the homepage');
+  res.sendFile(__dirname + '/index.html');
 });
+
 app.get('/contact', function(req,res){
   res.send('this is the contact page');
 });
 
 app.get('/404', function(req,res){
   res.send('404 Error - Not Found');
+});
+
+//Inject dynamic content
+app.get('/profile/:name', function(req,res){
+  //res.send('You are viewing the profile of ' + req.params.name);
+  //res.render('profile');
+  var data = {age: 29, job: 'coder'};
+  res.render('profile', {person: req.params.name, data: data });
 });
 
 //3. Listen to desired port
